52997bd7af0b2fdab99f9493396a7b4632d3e0e7
wesley27 <www.wesley2@gmail.com>
2014-12-11 23:31:15 -0500

    Uncommitted changes/updates

--- src/com/wesley27/selectivepvp/ReloadCommand.java
+++ src/com/wesley27/selectivepvp/ReloadCommand.java
@@ -6,40 +6,37 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class ReloadCommand implements CommandExecutor
-{
+public class ReloadCommand implements CommandExecutor {
 	SelectivePvP plugin;
 
-    ReloadCommand(SelectivePvP plugin) {
-        this.plugin = plugin;
-    }
+	ReloadCommand(SelectivePvP plugin) {
+		this.plugin = plugin;
+	}
 
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) 
-    {
-        String cmd = command.getName();
+	public boolean onCommand(CommandSender sender, Command command,
+			String label, String[] args) {
+		String cmd = command.getName();
 
-        if (cmd.equals("selectivepvp")) 
-        {
-            if (args.length >= 1)
-                if (args[0].equals("reload")) 
-                {
-                    if (sender instanceof Player) 
-                    {
-                        Player player = (Player) sender;
+		if (cmd.equals("selectivepvp")) {
+			if (args.length >= 1)
+				if (args[0].equals("reload")) {
+					if (sender instanceof Player) {
+						Player player = (Player) sender;
 
-                        if (!(player.hasPermission("selectivepvp.reload"))) 
-                        {
-                            player.sendMessage(ChatColor.RED + "[SelectivePvP] You don't have Permissions to do this.");
-                            return true;
-                        }
-                    }
+						if (!(player.hasPermission("selectivepvp.reload"))) {
+							player.sendMessage(ChatColor.RED
+									+ "[SelectivePvP] You don't have Permissions to do this.");
+							return true;
+						}
+					}
 
-                    plugin.reloadConfig();
-                    sender.sendMessage(ChatColor.RED + "[SelectivePvP] Config reloaded.");
+					plugin.reloadConfig();
+					sender.sendMessage(ChatColor.RED
+							+ "[SelectivePvP] Config reloaded.");
 
-                    return true;
-                }
-        }
-        return false;
-    }
+					return true;
+				}
+		}
+		return false;
+	}
 }
--- src/com/wesley27/selectivepvp/SelectivePvP.java
+++ src/com/wesley27/selectivepvp/SelectivePvP.java
@@ -12,75 +12,83 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class SelectivePvP extends JavaPlugin implements Listener
-{
+public class SelectivePvP extends JavaPlugin implements Listener {
 	Logger logger = Logger.getLogger("Minecraft");
 	final ArrayList<String> victims = new ArrayList<String>();
+	Timer t = new Timer();
 
-	public void onEnable() 
-	{
+	public void onEnable() {
 		logger.info("SelectivePvP Enabled");
 		getServer().getPluginManager().registerEvents(this, this);
-		
+
 		getCommand("selectivepvp").setExecutor(new ReloadCommand(this));
-		
+
 		loadConfiguration();
 	}
 
-	public void onDisable() 
-	{
+	public void onDisable() {
 		logger.info("SelectivePvP Disabled");
 	}
-	
-	public void loadConfiguration()
-	{
+
+	public void loadConfiguration() {
 		getConfig().options().copyDefaults(true);
 		saveDefaultConfig();
 	}
-	
+
 	@EventHandler
-	public void onPlayerAttack(EntityDamageByEntityEvent event)
-	{
-		if(event.getDamager() instanceof Player && event.getEntity() instanceof Player)
-		{
+	public void onPlayerAttack(EntityDamageByEntityEvent event) {
+		if (event.getDamager() instanceof Player
+				&& event.getEntity() instanceof Player) {
 			Player attacker = (Player) event.getDamager();
 			final Player victim = (Player) event.getEntity();
-			
-			if(!victims.contains(attacker.getName()))
-			{				
+
+			if (!victims.contains(attacker.getName())) {
 				String item = attacker.getItemInHand().getType().toString();
-				
-				if(!getConfig().getStringList("allowed-weapons").contains(item))
-				{
-					//attacker.sendMessage(ChatColor.RED + "[" + ChatColor.DARK_RED + "SelectivePvP" + ChatColor.RED + "] You cannot attack someone with this item!");
-					//victim.sendMessage(ChatColor.RED + "[" + ChatColor.DARK_RED + "SelectivePvP" + ChatColor.RED + "] " + ChatColor.DARK_RED + attacker.getName() + ChatColor.RED + " has tried to attack you, but didn't have a proper weapon!");
+
+				if (!getConfig().getStringList("allowed-weapons")
+						.contains(item)) {
+					// attacker.sendMessage(ChatColor.RED + "[" +
+					// ChatColor.DARK_RED + "SelectivePvP" + ChatColor.RED +
+					// "] You cannot attack someone with this item!");
+					// victim.sendMessage(ChatColor.RED + "[" +
+					// ChatColor.DARK_RED + "SelectivePvP" + ChatColor.RED +
+					// "] " + ChatColor.DARK_RED + attacker.getName() +
+					// ChatColor.RED +
+					// " has tried to attack you, but didn't have a proper weapon!");
 					event.setCancelled(true);
-				}
-				else if(getConfig().getStringList("allowed-weapons").contains(item))
-				{
-					if(getConfig().getBoolean("self-defence-enabled"))
-					{
+				} else if (getConfig().getStringList("allowed-weapons")
+						.contains(item)) {
+					if (getConfig().getBoolean("self-defence-enabled")) {
 						victims.add(victim.getName());
-						victim.sendMessage(ChatColor.RED + "[" + ChatColor.DARK_RED + "SelectivePvP" + ChatColor.RED + "] You've been attacked! You can use anything to defend yourself for " + getConfig().getInt("self-defence-time") + " seconds!");
-						
-						Timer t = new Timer();
-						t.schedule(new TimerTask()
-						{
+						victim.sendMessage(ChatColor.RED
+								+ "["
+								+ ChatColor.DARK_RED
+								+ "SelectivePvP"
+								+ ChatColor.RED
+								+ "] You've been attacked! You can use anything to defend yourself for "
+								+ getConfig().getInt("self-defence-time")
+								+ " seconds!");
+
+						t.schedule(new TimerTask() {
 							@Override
-							public void run()
-							{
-								victim.sendMessage(ChatColor.RED + "[" + ChatColor.DARK_RED + "SelectivePvP" + ChatColor.RED + "] You are no longer under attack.");
+							public void run() {
+								victim.sendMessage(ChatColor.RED + "["
+										+ ChatColor.DARK_RED + "SelectivePvP"
+										+ ChatColor.RED
+										+ "] You are no longer under attack.");
 								victims.remove(victim.getName());
 							}
 						}, (getConfig().getInt("self-defence-time") * 1000));
-						
-						if(victim.isDead())
-						{
+
+						if (victim.isDead()) {
 							t.cancel();
 							victims.remove(victim.getName());
 						}
 					}
 				}
+			} else if (victims.contains(attacker.getName())) {
+				t.cancel();
+				victims.remove(attacker.getName());
 			}
 		}
 	}
